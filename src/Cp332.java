e5d7110018f9548e26cbe0819722e7a1bf30673f
cfyhmyx <cfyhmyx@gmail.com>
2020-04-24 17:24:36 -0700

    graph, eulerian path

---
+++ src/Cp332.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/reconstruct-itinerary/
+
+import java.util.*;
+
+public class Cp332 {
+    public static void main(String args[]) {
+
+    }
+
+    // Main idea: Eulerian path algorithm
+    // Time: O(nlogn), nlogn: priority queue. Eulertian path algorithm o(n).
+    // Space: O(n)
+    public static List<String> findItinerary(List<List<String>> tickets) {
+        Map<String, PriorityQueue<String>> flights = new HashMap<>();
+        LinkedList<String> path = new LinkedList<>();
+        for (List<String> ticket : tickets) {
+            flights.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            flights.get(ticket.get(0)).add(ticket.get(1));
+        }
+        findEulerianPath(flights, path, "JFK");
+        return path;
+    }
+
+    private static void findEulerianPath(
+            Map<String, PriorityQueue<String>> flights, LinkedList<String> path, String departure) {
+        PriorityQueue<String> arrivals = flights.get(departure);
+        while (arrivals != null && !arrivals.isEmpty()) {
+            findEulerianPath(flights, path, arrivals.poll());
+        }
+        path.addFirst(departure);
+    }
+}
