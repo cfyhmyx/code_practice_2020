1a13bf01b24ae94fb0dfb94cc3fc0e282a9a342d
cfyhmyx <cfyhmyx@gmail.com>
2020-07-27 21:29:07 -0700

    tree, recursion

---
+++ src/Cp0235.java
@@ -0,0 +1,36 @@
+// https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+public class Cp0235 {
+    // Main idea: tree, recursion.
+    // Time: O(n), n is the number of tree node.
+    // Space: O(n).
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (root == p || root == q) return root;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) return root;
+        if (left != null) return left;
+        if (right != null) return right;
+        return null;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
